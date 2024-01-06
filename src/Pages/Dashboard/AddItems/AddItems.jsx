24ba88841a96cd31245aa8d84f5baa4fbadb2bd1
AbdullahAlHemel <abdullahalhemel100@gmail.com>
2024-01-06 13:17:49 +0600

    add items

--- src/Pages/Dashboard/AddItems/AddItems.jsx
+++ src/Pages/Dashboard/AddItems/AddItems.jsx
@@ -2,14 +2,16 @@ import { useForm } from "react-hook-form";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import { FaUtensils } from "react-icons/fa";
 import useAxiosPublic from "../../../Components/Hooks/useAxiosPublic";
+import useAxiosSecure from "../../../Components/Hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 
 const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
 const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`
 const AddItems = () => {
-    const { register, handleSubmit } = useForm()
-    const axiosPublic = useAxiosPublic()
-
+    const { register, handleSubmit, reset } = useForm()
+    const axiosPublic = useAxiosPublic();
+    const axiosSecure = useAxiosSecure();
     const onSubmit = async(data) => {
         console.log(data)
         //image upload to imgbb and then get an url
@@ -28,8 +30,22 @@ const AddItems = () => {
                 recipe: data.recipe,
                 image: res.data.data.display_url
             }
+            //
+            const menuRes = await axiosSecure.post('/menu', menuItem);
+            console.log(menuRes.data);
+            if(menuRes.data.insertedId){
+                // show success popup
+                reset();
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: `${data.name} is added to the menu.`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  });
+            }
         }
-        console.log(res.data)
+        console.log('with image url', res.data)
     }
     return (
         <div>
@@ -62,7 +78,7 @@ const AddItems = () => {
                        required
                        {...register("category")} 
                        className="select select-bordered w-full">
-                        <option disabled value="default" selected>Select a category</option>
+                        <option disabled value="default" >Select a category</option>
                         <option value='salad'>Salad</option>
                         <option value='pizza'>Pizza</option>
                         <option value='desert'>Desert</option>
---
+++ src/Pages/Dashboard/ManageItems/ManageItems.jsx
@@ -0,0 +1,90 @@
+import { IoIosTrash } from "react-icons/io";
+import UseMenu from "../../../Components/Hooks/UseMenu";
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import { FaEdit } from "react-icons/fa";
+
+const ManageItems = () => {
+    const [menu] = UseMenu();
+
+    const handleDelete = id => {
+        // Swal.fire({
+        //   title: "Are you sure to Delete?",
+        //   text: "You won't be able to revert this!",
+        //   icon: "warning",
+        //   showCancelButton: true,
+        //   confirmButtonColor: "#3085d6",
+        //   cancelButtonColor: "#d33",
+        //   confirmButtonText: "Yes, delete it!"
+        // }).then((result) => {
+        //   if (result.isConfirmed) {
+        //     axiosSecure.delete(`/carts/${id}`)
+        //     .then(res => {
+        //       if(res.data.deletedCount > 0){
+        //         refetch()
+        //         Swal.fire({
+        //           title: "Deleted!",
+        //           text: "Your file has been deleted.",
+        //           icon: "success"
+        //         });
+        //       }
+        //     })
+        //   }
+        // });
+      }
+
+    return (
+        <div>
+           <SectionTitle heading='Manage All Items' subHeading='Hurry Up'></SectionTitle>            
+       
+           <div className="overflow-x-auto">
+  <table className="table w-full mx-5">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>
+           #
+        </th>
+        <th>Image</th>
+        <th>Item Name</th>
+        <th>Price</th>
+        <th>Update</th>
+        <th>Delete</th>
+      </tr>
+    </thead>
+    <tbody>
+      { menu.map((item, index) =>  <tr key={item._id}>
+        <th>
+           {index + 1}
+        </th>
+        <td>
+          <div className="flex items-center gap-3">
+            <div className="avatar">
+              <div className="mask mask-squircle w-12 h-12">
+                <img src={item.image} alt="Avatar Tailwind CSS Component" />
+              </div>
+            </div>
+          </div>
+        </td>
+        <td>
+           <div>
+            <h2 className="font-semibold">{item.name}</h2>
+           </div>
+        </td>
+        <td>
+            <h2>$ {item.price}</h2>
+        </td>
+        <td>
+          <button className="btn btn-ghost btn-sm"><FaEdit className="text-green-400 "></FaEdit></button>
+        </td>
+        <td>
+        <button onClick={() => handleDelete(item._id)} className="btn btn-ghost btn-xs"><IoIosTrash className="text-2xl rounded-full text-red-400 hover:text-red-600"/></button>
+        </td>
+      </tr>    )}
+    </tbody>   
+  </table>
+</div>
+        </div>
+    );
+};
+
+export default ManageItems;
